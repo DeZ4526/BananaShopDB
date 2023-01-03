1169bb8a4e61e65a0bed1624fadec36c2c1cdfcd
DeZ4526 <karpov.k-r@yandex.ru>
2023-01-03 20:42:34 +0300

    Fix orders system

--- FUNCTIONS/FOR orders SYSTEM.sql
+++ FUNCTIONS/FOR orders SYSTEM.sql
@@ -1,3 +1,16 @@
+--Взять заказ
+CREATE FUNCTION Done_Order(_id_order bigint) RETURNS bool AS $$
+BEGIN
+	IF (SELECT Count(*) FROM Orders WHERE id_p_log = _id_order) = 1
+	THEN
+		UPDATE Orders SET is_done=true WHERE id_p_log = _id_order;
+		RETURN true;
+	ELSE
+		RETURN false;
+	END IF;
+END;
+$$ LANGUAGE plpgsql;
+
 --Выполнить заказ
 CREATE FUNCTION Done_Order(_id_order bigint) RETURNS bool AS $$
 BEGIN
@@ -13,26 +26,31 @@ $$ LANGUAGE plpgsql;
 
 --Добавить заказ
 CREATE FUNCTION Add_Order(_id_user int, _id_product int, _information text) RETURNS bool AS $$
+DECLARE
+    _id_ref bigint;
 BEGIN
 	IF ((SELECT Count(*) FROM users WHERE id_user = _id_user) = 1 AND
 	   (SELECT Count(*) FROM Products WHERE _id_product = id_product) = 1 AND
-	   (SELECT balance FROM Users WHERE  id_user = _id_user) >= (SELECT price FROM Products WHERE _id_product = id_product)) THEN
-		INSERT INTO Orders (id_user, id_product ,information, date_time, is_done) VALUES 
+	   (SELECT balance FROM Users WHERE id_user = _id_user) >= (SELECT price FROM Products WHERE _id_product = id_product)) THEN
+		
+		INSERT INTO Orders (id_user, id_product ,information, date_time, is_done) 
+		VALUES 
 		(_id_user, _id_product,_information, now(), false);
 		
-		SELECT Add_Balance_ref((SELECT id_referral FROM Users WHERE id_user = _id_user), 
-							   _id_user, 
-							   ((SELECT price FROM Products WHERE _id_product = id_product)/ 100 * ref_percent));
+		_id_ref = (SELECT id_referral FROM Users WHERE id_user = _id_user);
+		
+		UPDATE Users SET 
+		balance = balance - (SELECT price FROM Products WHERE _id_product = id_product)
+		WHERE id_user = _id_user;
+		
+		RETURN Add_Balance_ref(_id_ref, 
+							   'REF', 
+							   ((SELECT price FROM Products WHERE _id_product = id_product)/ 100 * (SELECT ref_percent FROM Users WHERE id_user =_id_ref)));
 		
 		--!UPDATE Users SET 
 		--balance = balance + (((SELECT price FROM Products WHERE _id_product = id_product))/ 100 * ref_percent), 
 		--ref_balance = ref_balance + ((SELECT price FROM Products WHERE _id_product = id_product)/ 100 * ref_percent) 
 		--WHERE id_user = (SELECT id_referral FROM Users WHERE id_user = _id_user);
-		
-		UPDATE Users SET 
-		balance = balance - (SELECT price FROM Products WHERE _id_product = id_product)
-		WHERE id_user = _id_user;
-		RETURN true;
 	ELSE
 		RETURN false;
 	END IF;
@@ -40,19 +58,18 @@ END;
 $$ LANGUAGE plpgsql;
 
 --Пополнить баланс через реферальную систему 
-CREATE FUNCTION Add_Balance_ref(_id_user int, _requisites text, _value numeric) RETURNS bool AS $$
+CREATE FUNCTION Add_Balance_ref(_id_user bigint, _requisites text, _value numeric) RETURNS bool AS $$
 BEGIN
-	IF ((SELECT Count(*) FROM users WHERE id_user = _id_user) = 0 AND
-	   (SELECT Count(*) FROM Purchase_methods WHERE id_purchase_method = id_p_method) = 0) THEN
+	IF (SELECT Count(*) FROM users WHERE id_user = _id_user) = 0 THEN
 		RETURN false;
 	END IF;
 	
 	IF(SELECT count(*) FROM Purchase_methods WHERE title='REFERAL_SYSTEM') = 0 THEN
-		SELECT Add_Purchase_method('REFERAL_SYSTEM', 'REFERAL_SYSTEM');
+		PERFORM Add_Purchase_method('REFERAL_SYSTEM', 'REFERAL_SYSTEM');
 	END IF;
 	
-	INSERT INTO Balance_log (id_user, id_p_method ,requisites ,value ,description ,date_time,use_bonus) VALUES 
-	(_id_user, (SELECT SUM(id_p_method) FROM Purchase_methods WHERE title='REFERAL_SYSTEM'),_requisites,_value,'REFERAL_SYSTEM', now(), false);
+	INSERT INTO Balance_log (id_user, id_p_method ,requisites ,value ,description ,date_time,use_bonus, first_add) VALUES 
+	(_id_user, (SELECT SUM(id_p_method) FROM Purchase_methods WHERE title='REFERAL_SYSTEM'),_requisites,_value,'REFERAL_SYSTEM', now(), false, false);
 	UPDATE users SET balance=balance+_value, ref_balance = ref_balance+_value WHERE id_user = _id_user;
 	RETURN true;
 END;
