0f0d49c5c1fdd0e5fa0555b70b99bcbdcd68aaad
NoneNameDeveloper <69014907+NoneNameDeveloper@users.noreply.github.com>
2023-01-06 23:36:27 +0300

    add date_time for add_user functions

--- FUNCTIONS/FOR Users TABLE.sql
+++ FUNCTIONS/FOR Users TABLE.sql
@@ -1,10 +1,10 @@
 --Добавить пользователя
-CREATE FUNCTION Add_User(_id_user bigint, _login text, user_type int, _balance numeric, _bonus int, _ref_percent int) RETURNS bool AS $$
+CREATE FUNCTION Add_User(_id_user bigint, user_type int, login text, _balance numeric, _bonus int, _ref_percent int) RETURNS bool AS $$
 BEGIN
-	IF ((SELECT Count(*) FROM Users WHERE _login = login) = 0 AND 
+	IF ((SELECT Count(*) FROM Users WHERE id_user = _id_user) = 0 AND 
 		(SELECT Count(*) FROM User_types WHERE id_type = user_type) = 1) THEN
-		INSERT INTO Users (id_user,login, id_type, balance, bonus, ref_percent, ref_balance, pur_balance) VALUES 
-		(_id_user,_login, user_type, _balance,_bonus, _ref_percent, 0, 0);
+		INSERT INTO Users (id_user, id_type, login, balance, bonus, date_time, ref_percent, ref_balance, pur_balance) VALUES 
+		(_id_user, user_type, login, _balance,_bonus, now(), _ref_percent, 0, 0);
 		RETURN true;
 	ELSE
 		RETURN false;
@@ -13,13 +13,13 @@ END;
 $$ LANGUAGE plpgsql;
 
 --Добавить пользователя с реферальной ссылки
-CREATE FUNCTION Add_User(_id_user bigint,_login text, user_type int, _balance numeric, _bonus int, _id_referral int, _ref_percent int) RETURNS bool AS $$
+CREATE FUNCTION Add_User(_id_user bigint, user_type int, login text, _balance numeric, _bonus int, _id_referral int, _ref_percent int) RETURNS bool AS $$
 BEGIN
-	IF ((SELECT Count(*) FROM Users WHERE _login = login) = 0 AND 
+	IF ((SELECT Count(*) FROM Users WHERE id_user = _id_user) = 0 AND 
 		(SELECT Count(*) FROM Users WHERE id_user = _id_referral) = 1 AND
 	   	(SELECT Count(*) FROM User_types WHERE id_type = user_type) = 1) THEN
-		INSERT INTO Users (id_user, login, id_type, balance, bonus, id_referral,ref_percent, ref_balance,  pur_balance) VALUES 
-		(_id_user, _login, user_type, _balance,_bonus, _id_referral, _ref_percent, 0, 0);
+		INSERT INTO Users (id_user, id_type, login, balance, bonus, date_time, id_referral,ref_percent, ref_balance,  pur_balance) VALUES 
+		(_id_user, user_type, login, _balance, _bonus, now(), _id_referral, _ref_percent, 0, 0);
 		RETURN true;
 	ELSE
 		RETURN false;
