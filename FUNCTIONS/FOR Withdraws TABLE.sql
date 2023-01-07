cac80e2fc20787a145456cbc0a13426102d38003
NoneNameDeveloper <69014907+NoneNameDeveloper@users.noreply.github.com>
2023-01-07 13:45:52 +0300

    Update FOR Withdraws TABLE.sql

--- FUNCTIONS/FOR Withdraws TABLE.sql
+++ FUNCTIONS/FOR Withdraws TABLE.sql
@@ -1,10 +1,10 @@
 --Добавить выплату
-create function Add_Withdraw(_id_user bigint, _pay_type text, _rekvisits text) returns bool as $$
+create function Add_Withdraw(_id_user bigint, _pay_type text, _rekvisits text, _value numeric) returns bool as $$
 begin
 	if ((select count(*) from withdraws where id_user = _id_user and is_done = true) = 0 AND
 		(SELECT Count(*) FROM users WHERE id_user = _id_user) = 1) then 
-		insert into withdraws (id_user, pay_type, rekvisits, date_time, is_done) values 
-		(_id_user, _pay_type, _rekvisits, now(), false);
+		insert into withdraws (id_user, pay_type, rekvisits, value, date_time, is_done) values 
+		(_id_user, _pay_type, _rekvisits, _value, now(), false);
 		return true;
 	else
 		return false;
